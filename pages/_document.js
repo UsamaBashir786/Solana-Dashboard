d8369e5813c1f19b39d17f769115d55fff60b8b7
Usama Bashir <165399383+UsamaBashir786@users.noreply.github.com>
2026-01-14 14:42:17 +0500

    new

---
+++ pages/_document.js
@@ -0,0 +1,30 @@
+// pages/_document.js
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        {/* Favicon for all devices */}
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#9945ff" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        
+        {/* Meta tags for PWA */}
+        <meta name="msapplication-TileColor" content="#9945ff" />
+        <meta name="msapplication-config" content="/browserconfig.xml" />
+        <meta name="theme-color" content="#9945ff" />
+        
+        {/* PWA manifest */}
+        <link rel="manifest" href="/manifest.json" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
