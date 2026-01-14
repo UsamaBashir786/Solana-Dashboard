316805c65410813cb33876af4b9b5cf05915cdf5
Usama Bashir <165399383+UsamaBashir786@users.noreply.github.com>
2026-01-14 15:07:51 +0500

    new

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head>
         {/* Primary favicon - will be overridden by index.js if needed */}
-        <link rel="icon" href="/Solana%20Logomark%20-%20Color.svg" type="image/svg+xml" />
+        <link rel="icon" href="/Solana-Logomark-Color.svg" type="image/svg+xml" />
         
         {/* PWA Manifest */}
         <link rel="manifest" href="/site.webmanifest" />
